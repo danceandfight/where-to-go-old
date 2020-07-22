873c484211750c336ca2b974b8ec27c7e45e0709
danceandfight <danceandfight@gmail.com>
2020-07-22 14:50:04 +0300

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from where_to_go.views import show_index_page
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_index_page),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
