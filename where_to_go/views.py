3ac6aec6bfe2f86193e3ffbc1eb915afdec9ed02
danceandfight <danceandfight@gmail.com>
2020-07-24 14:49:27 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from places.models import Place
+
+def serialize_place(place):
+    return {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.id,
+            "detailsUrl": None
+          }
+        }
 
 def show_index_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    places_geojson = {'type': 'FeatureCollection'}
+    places = Place.objects.all()
+    places_content = [serialize_place(place) for place in places]
+    places_geojson['features'] = places_content
+    context = {'places_geojson': places_geojson}
+    return render(request, 'index.html', context=context)
