4bf2d7efd09c1a25553871faca1c90497d9d4ce4
danceandfight <danceandfight@gmail.com>
2020-07-31 14:20:21 +0300

    Add ability to edit PlaceImage from Place admin page

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 
-admin.site.register(Place)
-# Register your models here.
-admin.site.register(PlaceImage)
+class PlaceImageInline(admin.TabularInline):
+	model = PlaceImage
+	ordering = ('number',)
+
+class PlaceAdmin(admin.ModelAdmin):
+	inlines = [
+	    PlaceImageInline,
+	]
+
+class PlaceImageAdmin(admin.ModelAdmin):
+	ordering = ('number',)
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(PlaceImage, PlaceImageAdmin)
