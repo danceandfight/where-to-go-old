6726ea1a4c7c6bbd9f0ffeba048c5e8e644fc0cd
danceandfight <danceandfight@gmail.com>
2020-08-06 15:15:03 +0300

    Move static folder in places

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -56,11 +56,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'where_to_go.urls'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -128,5 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "places/static"),
+    #'/var/www/static/',
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
