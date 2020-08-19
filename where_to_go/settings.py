c30186af6e5bc60a1bb9c8c973dc879d28147060
danceandfight <danceandfight@gmail.com>
2020-08-19 15:13:28 +0300

    Remove commented code

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "places/static"),
-    #'/var/www/static/',
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
